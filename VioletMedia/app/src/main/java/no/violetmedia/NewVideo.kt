633a8f52c030d88daff874d04330165b0671c73e
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2023-10-12 15:24:32 +0200

    Created Activities for UI

---
+++ VioletMedia/app/src/main/java/no/violetmedia/NewVideo.kt
@@ -0,0 +1,11 @@
+package no.violetmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewVideo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_video)
+    }
+}
---
+++ VioletMedia/app/src/main/java/no/violetmedia/StoredVideo.kt
@@ -0,0 +1,11 @@
+package no.violetmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoredVideo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stored_video)
+    }
+}
