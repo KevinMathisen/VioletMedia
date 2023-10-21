862d1a95fa89cc6346c851617b4f660af106e3aa
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-10-21 11:11:27 +0200

    feat: Update gradle.kts to include Exoplayer

--- VioletMedia/app/build.gradle.kts
+++ VioletMedia/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "no.violetmedia"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -48,4 +48,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Exoplayer
+    implementation("androidx.media3:media3-exoplayer:1.1.1")
+    implementation("androidx.media3:media3-exoplayer-dash:1.1.1")
+    implementation("androidx.media3:media3-ui:1.1.1")
 }
