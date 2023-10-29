8379b4a60177bcb302aecaafdae3038016d34416
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-10-29 10:37:25 +0100

    feat: add functionality which support rotation of screen by managing savedInstanceState

--- VioletMedia/app/src/main/java/no/violetmedia/VideoPlayer.kt
+++ VioletMedia/app/src/main/java/no/violetmedia/VideoPlayer.kt
@@ -3,29 +3,66 @@ package no.violetmedia
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.media3.exoplayer.ExoPlayer
 import no.violetmedia.databinding.ActivityVideoPlayerBinding
 import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.SimpleExoPlayer
 
 class VideoPlayer : AppCompatActivity() {
 
     private lateinit var binding: ActivityVideoPlayerBinding
+    private lateinit var player: ExoPlayer
+    private var playbackPos: Long = 0
+    private var currentWindow: Int = 0
+    private var playWhenReady: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val player = ExoPlayer.Builder(this).build()
-        val playerView = binding.playerView
+        if (savedInstanceState != null) {
+            playbackPos = savedInstanceState.getLong("playbackPos")
+            currentWindow = savedInstanceState.getInt("currentWindow")
+            playWhenReady = savedInstanceState.getBoolean("playWhenReady")
+        }
 
+        initializeExoPlayer("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd")
+    }
+
+    private fun initializeExoPlayer(url: String) {
+        player = ExoPlayer.Builder(this).build()
+        val playerView = binding.playerView
         playerView.player = player
 
-        val uri = Uri.parse("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd")
+        val uri = Uri.parse(url)
         val mediaItem = MediaItem.fromUri(uri)
         player.setMediaItem(mediaItem)
 
         player.prepare()
-        player.play()
+        player.playWhenReady = playWhenReady
+        player.seekTo(currentWindow, playbackPos)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putLong("playbackPos", player.currentPosition)
+        outState.putInt("currentWindow", player.currentMediaItemIndex)
+        outState.putBoolean("playWhenReady", player.playWhenReady)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        playbackPos = player.currentPosition
+        currentWindow = player.currentMediaItemIndex
+        playWhenReady = player.playWhenReady
+        player.release()
     }
 }
