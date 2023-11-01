27591245a810b988b5c6374337dda45237371605
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2023-11-01 12:08:31 +0100

    Added speech text for StoredVideo

--- VioletMedia/app/src/main/java/no/violetmedia/StoredVideo.kt
+++ VioletMedia/app/src/main/java/no/violetmedia/StoredVideo.kt
@@ -1,24 +1,69 @@
 package no.violetmedia
 
+import android.Manifest
+import android.speech.RecognizerIntent
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.view.inputmethod.InputMethodManager
 import android.widget.Adapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import no.violetmedia.databinding.ActivityStoredVideoBinding
-import android.content.Context
-import android.view.inputmethod.InputMethodManager
+import java.util.Locale
+
 class StoredVideo : AppCompatActivity() {
 
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var spokenText: String
     private lateinit var binding: ActivityStoredVideoBinding
     private lateinit var adapter: Adapter
+
+    companion object {
+        private const val REQUEST_SPEECH_RECOGNITION = 1
+    }
+    private var filter: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStoredVideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status != TextToSpeech.ERROR) {
+                // Set the language, if needed
+                textToSpeech.language = Locale.US // You can change this as needed
+            }
+        }
         val videos = VideoDataManager.getVideos(this)
+
+
+        requestMicrophonePermission()
+
+        binding.btnSpeech.setOnClickListener {
+            startSpeechRecognition()
+        }
+
+        textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+            override fun onDone(utteranceId: String?){
+                spokenText = utteranceId ?: ""
+                filter = spokenText
+                filterVideos(filter)
+
+            }
+            override fun onError(utteranceId: String?) {
+                // Handle any errors if needed
+            }
+
+            override fun onStart(utteranceId: String?) {
+                // Handle the start of speech if needed
+            }
+        })
 
         binding.btnBack.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
@@ -27,7 +72,7 @@ class StoredVideo : AppCompatActivity() {
 
         binding.btnFilter.setOnClickListener {
             hideKeyboard()
-            var filter = binding.etFilter.text.toString()
+            val filter = binding.etFilter.text.toString()
             filterVideos(filter)
         }
 
@@ -43,6 +88,32 @@ class StoredVideo : AppCompatActivity() {
         binding.rvVideos.adapter = adapter
         binding.rvVideos.layoutManager = LinearLayoutManager(this)
 
+    }
+
+    private fun requestMicrophonePermission() {
+        val permission = Manifest.permission.RECORD_AUDIO
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), REQUEST_SPEECH_RECOGNITION)
+        }
+    }
+
+    private fun startSpeechRecognition() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        startActivityForResult(intent, REQUEST_SPEECH_RECOGNITION)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_SPEECH_RECOGNITION && resultCode == RESULT_OK) {
+            val results = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (!results.isNullOrEmpty()) {
+                spokenText = results[0]
+                filter = spokenText
+                filterVideos(filter)
+            }
+        }
     }
     private fun filterVideos(query : String?) {
 
@@ -79,4 +150,13 @@ class StoredVideo : AppCompatActivity() {
             inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
         }
     }
+
+    override fun onDestroy(){
+        if (textToSpeech.isSpeaking){
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown()
+        super.onDestroy()
+    }
 }
+
