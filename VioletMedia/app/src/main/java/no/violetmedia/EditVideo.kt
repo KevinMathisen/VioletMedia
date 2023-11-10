a953f7a512df472ef82c123ccec242b88bd87e58
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-11-10 19:01:47 +0100

    docs: write documentation and update broadcast action name

--- VioletMedia/app/src/main/java/no/violetmedia/EditVideo.kt
+++ VioletMedia/app/src/main/java/no/violetmedia/EditVideo.kt
@@ -8,85 +8,123 @@ import androidx.appcompat.app.AlertDialog
 import no.violetmedia.databinding.ActivityEditVideoBinding
 import no.violetmedia.databinding.ConfirmDeleteDialogBinding
 
-
+/**
+ * Activity class for editing a given video
+  */
 class EditVideo : AppCompatActivity() {
     private lateinit var binding: ActivityEditVideoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set up binding
         binding = ActivityEditVideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        // Get the name of the video we want to edit
         val orgName = intent.getStringExtra("source") ?: "N/A"
 
+        // Get all videos stored, and retrive the video we want to edit
         var videos = VideoDataManager.getVideos(applicationContext)
         val video = videos.find { it.name == orgName }
 
+        // Get the video details or default values
         val source = video?.source ?: "N/A"
         val description = video?.description ?: "N/A"
         val subSource = video?.subtitle
 
-        binding.tvEdit.text = "Edit ${orgName}"
 
+        // Set header text
+        binding.tvEdit.text = "Edit ${orgName}"
+        // Set text of name, description, and url fields
         binding.etNameEdit.setText(orgName)
         binding.etDescriptionEdit.setText(description)
         binding.etUrlEdit.setText(source)
 
+        // Set suburl text or provide hint if the value was found
         if (subSource != null) {
             binding.etSubUrlEdit.setText(subSource)
         } else {
             binding.etSubUrlEdit.hint = "Subtitle Url not set"
         }
 
+
+        // Save video with new details on button click
         binding.btnEditSave.setOnClickListener {
+
+            // Get all values entered by the user
             val newName = binding.etNameEdit.text.toString()
             val newDesc = binding.etDescriptionEdit.text.toString()
             val newSource = binding.etUrlEdit.text.toString()
             val subtitleText = binding.etSubUrlEdit.text.toString()
             val subtitle = if (subtitleText != "") subtitleText else null
 
+            // Create a new video with the given details
             val newVideo = VideoData(newName, newDesc, newSource, subtitle)
+            // Replace the video edited in the video list
             videos = videos.map { if (it.name == orgName) newVideo else it }.toMutableList()
 
+            // Save the updated videolist with the edited video
             VideoDataManager.saveVideos(applicationContext, videos)
 
+            // Inform user of success
             Toast.makeText(this, "Video saved!", Toast.LENGTH_SHORT).show()
         }
 
-        // Set an OnClickListener for the "Delete" button
+        // Ask for confirmation of deletion on delete button click
         binding.btnEditDelete.setOnClickListener {
             askForDeleteConfirmation(orgName)
         }
 
-        // Set an OnClickListener for the "Back" button
+        // Update videolist and finish activity on back button click
         binding.btnEditBack.setOnClickListener {
-            val intent = Intent("BroadcastReceiver")
+            // Create and send broadcast for refreshing videos displayed in the storedvideo activity
+            val intent = Intent("RefreshStoredVideos")
             sendBroadcast(intent)
+
             finish()
         }
 
     }
 
-    fun askForDeleteConfirmation(videoName: String) {
-        val binding = ConfirmDeleteDialogBinding.inflate(layoutInflater)
+    /**
+     * Method which asks the user for confirmation when trying to delete a video.
+     * If they click yes, delete the video.
+     *
+     * @param videoName Name of video to delete
+     */
+    private fun askForDeleteConfirmation(videoName: String) {
 
+        // Create and get binding reference to a conformation dialog
+        val binding = ConfirmDeleteDialogBinding.inflate(layoutInflater)
         val confirmView = AlertDialog.Builder(this).setView(binding.root).create()
 
+        // Dismiss dialog on cancel button click
         binding.cancelButton.setOnClickListener {
             confirmView.dismiss()
         }
 
+        // Delete video given on confirm button click
         binding.confirmButton.setOnClickListener {
+
+
+
+            // Get videos, remove selected video, and save the new list
             var videos = VideoDataManager.getVideos(applicationContext)
             videos = videos.filterNot { it.name == videoName }.toMutableList()
             VideoDataManager.saveVideos(applicationContext, videos)
 
+            // Dismiss the dialog
             confirmView.dismiss()
+
+            // Create and send broadcast for refreshing videos displayed in the storedvideo activity
             val intent = Intent("BroadcastReceiver")
             sendBroadcast(intent)
+
             finish()
         }
 
+        // Show the confirmDialog
         confirmView.show()
     }
 }
