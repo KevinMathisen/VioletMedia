7e099d38fa1b767b1a13bf1be4aeae2fdf8f7637
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-11-10 19:33:59 +0100

    docs: add comments to about activity

--- VioletMedia/app/src/main/java/no/violetmedia/About.kt
+++ VioletMedia/app/src/main/java/no/violetmedia/About.kt
@@ -5,25 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import no.violetmedia.databinding.ActivityAboutBinding
 
+/**
+ * Activity which displays information about the application
+ */
 class About : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set up binding
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set the names
         binding.tvAbout1.text = getString(R.string.names)
 
+        // Set the instruction text
         binding.tvAbout2.text = getString(R.string.about)
 
-        // Set an OnClickListener for the "Back" button
+        // Finish activity on "Back" button click
         binding.btnBack.setOnClickListener {
             finish()
         }
-
-
 
     }
 }
