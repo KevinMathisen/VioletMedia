cb1201ec233de6779c28eeb5ea17d3b5de5bac93
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-11-10 19:10:19 +0100

    docs: comments

--- VioletMedia/app/src/main/java/no/violetmedia/MainActivity.kt
+++ VioletMedia/app/src/main/java/no/violetmedia/MainActivity.kt
@@ -4,27 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import no.violetmedia.databinding.ActivityMainBinding
+
+/**
+ * Activity class for the front page
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set up binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set an OnClickListener for the "About" button
+        // Launch about activity on "About" button click
         binding.btnAbout.setOnClickListener {
             val intent = Intent(this, About::class.java)
             startActivity(intent)
         }
 
-        // Set an OnClickListener for the "New Video" button
+        //  Launch new video activity on "new video" button click
         binding.btnNewVideo.setOnClickListener {
             val intent = Intent(this, NewVideo::class.java)
             startActivity(intent)
         }
 
-        // Set an OnClickListener for the "Your Video" button
+        // Launch Stored video activity on "Stored Videos" button click
         binding.btnStoredVideo.setOnClickListener {
             val intent = Intent(this, StoredVideo::class.java)
             startActivity(intent)
